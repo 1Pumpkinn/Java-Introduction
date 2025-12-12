f98fac3c71661e28574a3492bc141f73576d87c2
Pumpkin <wishsmp1002@gmail.com>
2025-12-12 19:09:41 +1100

    switchStatements

---
+++ src/net/saturn/practice/switchStatements.java
@@ -0,0 +1,30 @@
+package net.saturn.practice;
+
+public class switchStatements {
+    public static void main(String[] args) {
+
+        int playerScores = 1;
+
+        switch (playerScores) {
+            case 1: System.out.println("Player 1 Has The Most Points"); break;
+            case 2: System.out.println("Player 2 Has The Most Points"); break;
+            case 3: System.out.println("Player 3 Has The Most Points"); break;
+            case 4: System.out.println("Player 4 Has The Most Points"); break;
+        }
+        int bestStudent = 2;
+
+        switch (bestStudent) {
+            case 1: System.out.println("Magnet108"); break;
+            case 2: System.out.println("SantaEevee"); break;
+            case 3: System.out.println("___Paanda"); break;
+            case 4: System.out.println("Acespeke"); break;
+            case 5: System.out.println("___Pumpkinn"); break;
+            case 6: System.out.println("CrimsonSolider"); break;
+            case 7: System.out.println("3Drop"); break;
+            case 8: System.out.println("Thecaketeen"); break;
+            case 9: System.out.println("Sushi"); break;
+            case 10: System.out.println("VishNEE"); break;
+        }
+
+    }
+}
